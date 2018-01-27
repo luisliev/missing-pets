9a1d99a69ab4c8f0c043903868daf91cef5dee8d
Luis Antonio Liévano Ayala <luis.lievano94@gmail.com>
2018-01-27 02:37:56 -0600

    Update app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAMZ66yN83fcsZc8owd2mL6TKkP9P3MEmE",
-    authDomain: "missingpets-37b6f.firebaseapp.com",
-    databaseURL: "https://missingpets-37b6f.firebaseio.com",
-    projectId: "missingpets-37b6f",
-    storageBucket: "missingpets-37b6f.appspot.com",
-    messagingSenderId: "716450562984"
+    apiKey: "-",
+    authDomain: -",
+    databaseURL: "-",
+    projectId: "-",
+    storageBucket: "-",
+    messagingSenderId: "-"
   };
